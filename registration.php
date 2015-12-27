2830092629cf1c6f386d56cd5b5a4c624dcf984c
Damián Culotta <damianculotta@gmail.com>
2015-12-27 18:32:23 -0300

    Initial commit.

---
+++ registration.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Dc_SampleModule
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Dc
+ * @package    Dc_SampleModule
+ * @copyright  Copyright (c) 2015 Damián Culotta. (http://www.damianculotta.com.ar/)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Dc_SampleModule',
+    __DIR__
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
