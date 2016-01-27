3247ecd970047de4f372b80d8df8d52af6c581bf
Damián Culotta <damianculotta@gmail.com>
2016-01-27 13:48:02 -0300

    Fixed module name definition.

--- registration.php
+++ registration.php
@@ -16,7 +16,7 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Dc_SampleModule',
+    'Barbanet_SampleModule',
     __DIR__
 );
 
