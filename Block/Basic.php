c45408616854e6a616d4602ee504ad3ddf5b3881
Damián Culotta <damianculotta@gmail.com>
2016-02-07 18:40:12 -0300

    Added custom block and template to be used with the first controller.

---
+++ Block/Basic.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Barbanet_SampleModule
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Barbanet
+ * @package    Barbanet_SampleModule
+ * @copyright  Copyright (c) 2015-2016 Damián Culotta. (http://www.damianculotta.com.ar/)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Barbanet\SampleModule\Block;
+
+use Magento\Framework\View\Element\Template;
+
+/**
+ * Basic SampleModule content block
+ */
+class Basic extends Template
+{
+
+    /**
+     * @param Template\Context $context
+     * @param array $data
+     */
+    public function __construct(Template\Context $context, array $data = [])
+    {
+        parent::__construct($context, $data);
+        $this->_isScopePrivate = true;
+    }
+
+    /**
+     * Returns a dummy string.
+     *
+     * @return string
+     */
+    public function getDummyString()
+    {
+        return 'Another line without translation defined into the block.';
+    }
+
+}
