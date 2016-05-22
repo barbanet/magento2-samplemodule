d20c5b95667580a416a49d75d9245196e01d0f4c
Damián Culotta <damianculotta@gmail.com>
2016-05-22 00:57:47 -0300

    Added Install and Upgrade schema scripts.

---
+++ Setup/InstallSchema.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Barbanet_SampleModule
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Barbanet
+ * @package    Barbanet_SampleModule
+ * @copyright  Copyright (c) 2015-2016 Damián Culotta. (http://www.damianculotta.com.ar/)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Barbanet\SampleModule\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\DB\Adapter\AdapterInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class InstallSchema implements InstallSchemaInterface
+{
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+
+        $installer->startSetup();
+
+        /**
+         * Create table 'samplemodule_table'
+         */
+        $table = $installer->getConnection()->newTable(
+            $installer->getTable('barbanet_samplemodule')
+        )->addColumn(
+            'row_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+            null,
+            ['identity' => true, 'nullable' => false, 'primary' => true],
+            'Row ID'
+        )->addColumn(
+            'title',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            255,
+            ['nullable' => false],
+            'Row Title'
+        )->addColumn(
+            'content',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            '2M',
+            [],
+            'Row Content'
+        )->addColumn(
+            'creation_time',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null,
+            ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
+            'Row Creation Time'
+        )->addColumn(
+            'update_time',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null,
+            ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE],
+            'Row Modification Time'
+        )->addColumn(
+            'is_active',
+            \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+            null,
+            ['nullable' => false, 'default' => '1'],
+            'Is Row Active'
+        )->addIndex(
+            $setup->getIdxName(
+                $installer->getTable('barbanet_samplemodule'),
+                ['title', 'content'],
+                AdapterInterface::INDEX_TYPE_FULLTEXT
+            ),
+            ['title', 'content'],
+            ['type' => AdapterInterface::INDEX_TYPE_FULLTEXT]
+        )->setComment(
+            'Barbanet SampleModule Table'
+        );
+        $installer->getConnection()->createTable($table);
+
+        $installer->endSetup();
+    }
+
+}
---
+++ Setup/UpgradeSchema.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Barbanet_SampleModule
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Barbanet
+ * @package    Barbanet_SampleModule
+ * @copyright  Copyright (c) 2015-2016 Damián Culotta. (http://www.damianculotta.com.ar/)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Barbanet\SampleModule\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\UpgradeSchemaInterface;
+
+
+class UpgradeSchema implements UpgradeSchemaInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        if (version_compare($context->getVersion(), '2.7.0', '<')) {
+            $setup->getConnection()->addColumn(
+                $setup->getTable('barbanet_samplemodule'),
+                'description',
+                [
+                    'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    'length' => 255,
+                    'nullable' => false,
+                    'comment' => 'Row description'
+                ]
+            );
+        }
+
+        $setup->endSetup();
+    }
+}
