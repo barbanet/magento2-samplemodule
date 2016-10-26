89e606cd199948e0302adea96e5c969eb4701ae9
Damián Culotta <damianculotta@gmail.com>
2016-10-26 00:01:13 -0300

    Added Console command.

---
+++ Console/Hello.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Barbanet_SampleModule
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Barbanet
+ * @package    Barbanet_SampleModule
+ * @copyright  Copyright (c) 2015-2016 Damián Culotta. (http://www.damianculotta.com.ar/)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Barbanet\SampleModule\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class Hello
+ * @package Barbanet\SampleModule\Console
+ */
+class Hello extends Command
+{
+
+    protected function configure()
+    {
+        $this->setName('samplemodule:hello');
+        $this->setDescription('Barbanet_SampleModule Hello sample command');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln("My Sample Module message: Hello World!");
+    }
+
+}
