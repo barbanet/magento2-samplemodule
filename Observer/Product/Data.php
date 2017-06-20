e5cbcafde15ab351c116e4a044509e560551fae1
Damián Culotta <damianculotta@gmail.com>
2017-06-20 00:35:13 -0300

    Added Observer example.

---
+++ Observer/Product/Data.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Barbanet_SampleModule
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Barbanet
+ * @package    Barbanet_SampleModule
+ * @copyright  Copyright (c) 2015-2017 Damián Culotta. (http://www.damianculotta.com.ar/)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Barbanet\SampleModule\Observer\Product;
+
+use Magento\Framework\Event\ObserverInterface;
+
+class Data implements ObserverInterface
+{
+
+    /**
+     * @param \Magento\Framework\Event\Observer $observer
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $product = $observer->getProduct();
+
+        $originalName = $product->getName();
+
+        $modifiedName = $originalName . ' - MODIFICADO POR EL OBSERVER';
+
+        $product->setName($modifiedName);
+    }
+
+}
