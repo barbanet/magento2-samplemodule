4037f1e10459db3a65ad1aae8941004b1e713f51
Damián Culotta <damianculotta@gmail.com>
2017-06-26 23:12:16 -0300

    Added Logger examples.

---
+++ Console/Logger.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Barbanet_SampleModule
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Barbanet
+ * @package    Barbanet_SampleModule
+ * @copyright  Copyright (c) 2015-2017 Damián Culotta. (http://www.damianculotta.com.ar/)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace Barbanet\SampleModule\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class Logger
+ * @package Barbanet\SampleModule\Console
+ */
+class Logger extends Command
+{
+
+    /**
+     * @var
+     */
+    private $logger;
+
+    /**
+     * @param \Psr\Log\LoggerInterface $logger
+     * @param null $name
+     */
+    public function __construct(
+        \Psr\Log\LoggerInterface $logger,
+        $name = null
+    ) {
+        $this->logger = $logger;
+        parent::__construct($name);
+    }
+
+    protected function configure()
+    {
+        $this->setName('samplemodule:logger');
+        $this->setDescription('Barbanet_SampleModule Logger test command');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->logger->alert('Mensaje Alerta');
+        $this->logger->critical('Mensaje Crítico');
+        $this->logger->debug('Mensaje Debug');
+        $this->logger->emergency('Mensaje Emergencia');
+        $this->logger->error('Mensaje Error');
+        $this->logger->info('Mensaje Info');
+        $this->logger->notice('Mensaje Notice');
+        $this->logger->warning('Mensaje Warning');
+
+        $this->logger->log(\Psr\Log\LogLevel::INFO, 'Mensaje Log');
+
+    }
+
+}
